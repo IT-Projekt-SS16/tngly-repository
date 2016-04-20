ea88633141226520283f05c96a42691efd26f248
Dominik Dach <Dominik Dach@IBM196-PBATMVB>
2016-04-20 14:55:24 +0200

    Kurzupdate da Klassen nicht mit ins RemoteRepository übernommen wurden.

---
+++ tngly-project/src/de/hdm/wi7/client/Update.java
@@ -0,0 +1,35 @@
+package de.hdm.wi7.client;
+
+import com.google.gwt.user.client.ui.*;
+
+public abstract class Update extends VerticalPanel{
+
+	  public void onLoad() {
+	 
+	    super.onLoad();
+
+	    this.add(this.createHeadline(this.getHeadlineText()));
+
+	    this.run();
+	  }
+
+	 
+	  protected HTML createHeadline(String text) {
+	    HTML content = new HTML(text);
+	    content.setStylePrimaryName("tngly-headline");
+	    return content;
+	  }
+
+	  
+	  protected void append(String text) {
+	    HTML content = new HTML(text);
+	    content.setStylePrimaryName("tngly-simpletext");
+	    this.add(content);
+	  }
+
+	  protected abstract String getHeadlineText();
+
+	  protected abstract void run();
+	}
+
+
