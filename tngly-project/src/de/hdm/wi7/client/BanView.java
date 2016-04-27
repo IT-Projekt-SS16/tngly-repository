0306e114722757f6b891e790a69bc3632781e31b
Esra <Esra@Esra-VAIO>
2016-04-27 16:52:03 +0200

    BanView überarbeitet/ausgestattet bzw. Liste und Remove-Button erstellt

--- tngly-project/src/de/hdm/wi7/client/BanView.java
+++ tngly-project/src/de/hdm/wi7/client/BanView.java
@@ -1,25 +1,68 @@
 package de.hdm.wi7.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.RootPanel;
+
 public class BanView extends Update{
-	/**
-	   * Jeder Showcase besitzt eine einleitende Überschrift, die durch diese
-	   * Methode zu erstellen ist.
-	   * 
-	   * @see Showcase#getHeadlineText()
-	   */
-	  @Override
+
+
 	  protected String getHeadlineText() {
 	    return "Ban View";
 	  }
+	 
 
-	  /**
-	   * Jeder Showcase muss die <code>run()</code>-Methode implementieren. Sie ist
-	   * eine "Einschubmethode", die von einer Methode der Basisklasse
-	   * <code>ShowCase</code> aufgerufen wird, wenn der Showcase aktiviert wird.
-	   */
-	  @Override
 	  protected void run() {
-		  this.append("Here you will see the ban's");
+		  this.append("Here you will see your Favourits");
 		  
-	  }
+		  FlexTable t = new FlexTable();
+		  
+		  
+		  t.setText(0, 0, "Philly Vanilli");
+		  t.setWidget(0, 1, new Button("remove"));
+		  
+		  
+		  
+		  t.setText(1, 0, "Kevin Jaeger");
+		  t.setWidget(1, 1, new Button("remove"));
+		  
+		  
+		  t.setText(2, 0, "Hans Wurst");
+		  t.setWidget(2, 1, new Button("remove"));
+		  
+		  
+		  t.setText(3, 0, "Mr. Cheese");
+		  t.setWidget(3, 1, new Button("remove"));
+		  
+		  RootPanel.get("Details").add(t);
+		 
+			        
+			      }
 }
+		    
+	 
+		    
+	  
+		    
+		    
+		    
+  
+
+		    
+		  
+		
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+
+
+
