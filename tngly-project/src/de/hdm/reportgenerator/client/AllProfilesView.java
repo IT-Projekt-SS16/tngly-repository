b38f416158b6ff35946051ceaa6001457679087b
Dach <Dach@IBM444-PF00ZG0A>
2016-05-04 13:46:33 +0200

    ReportgeneratorView eingeführt und ausführbar gemacht

---
+++ tngly-project/src/de/hdm/reportgenerator/client/AllProfilesView.java
@@ -0,0 +1,29 @@
+package de.hdm.reportgenerator.client;
+
+
+
+public class AllProfilesView extends UpdateReportGenerator{
+
+	/**
+	 * Jeder Showcase besitzt eine einleitende Überschrift, die durch diese
+	 * Methode zu erstellen ist.
+	 * 
+	 * @see Showcase#getHeadlineText()
+	 */
+	@Override
+	protected String getHeadlineText() {
+		return "HERE YOU CAN SEE ALL PROFILES";
+	}
+
+	/**
+	 * Jede View muss die <code>run()</code>-Methode implementieren. Sie ist
+	 * eine "Einschubmethode", die von einer Methode der Basisklasse
+	 * <code>Update</code> aufgerufen wird, wenn der View aktiviert wird.
+	 */
+	@Override
+	protected void run() {
+		
+	
+		
+}
+}
